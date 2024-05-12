afb0fde7aa2f5c4d20de115a58967024f488227e
Dantedevil10 <jonhjooj2907@gmail.com>
2024-05-12 09:46:30 -0300

    Criação do environment

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 
 @Injectable({
     providedIn:"root"
 })
 export class GamesService{
+    
+    constructor(private HttpClient:HttpClient){
+     
+    }
+    ObterJogos(){
+
+    }
 
 }
