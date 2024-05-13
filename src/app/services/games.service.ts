5b2c0733ca743d817382ae5a9b5b525bcde194a8
Dantedevil10 <jonhjooj2907@gmail.com>
2024-05-13 09:31:58 -0300

    Leve Termino Da API Mas Não Pronta

--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import { environment } from '../../environments/environment';
+import { GamesRe } from '../models/games.model';
 
 @Injectable({
     providedIn:"root"
 })
 export class GamesService{
+
+    private url = environment.api
     
     constructor(private HttpClient:HttpClient){
      
     }
     ObterJogos(){
-
+        return this.HttpClient.get<GamesRe[]>(this.url + '/games');
     }
 
 }
